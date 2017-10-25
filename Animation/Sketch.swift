b195d253fe7915dbef0a139f9cda7fd55885f320
Lee, Dakyum <dlee@lcs-mba3088.lcs.on.ca>
2017-10-25 10:24:49 -0400

    moving dot

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,9 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    // Chane in position
+    var dx : Int // difference in x
+    
     // This function runs once
     override init() {
         
@@ -16,17 +19,31 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
+        
+        // Set the difference for x
+        dx = 3
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        // Clear the background
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
+        
         // Change position
-        x += 1
+        x += dx
+        
+        // Check the position and reverse course
+        // if we go off the right edge of the screen
+        if x > 500 {
+            dx = -1
+        }
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.black
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
     }
