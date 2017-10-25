b6521a507b62345e9734a66f65fab5afc3c539af
Lee, Dakyum <dlee@lcs-mba3088.local>
2017-10-25 11:01:04 -0400

    Finish Four Corners Challenge

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,9 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    // Chane in position
+    var offset : Int // difference in x
+    
     // This function runs once
     override init() {
         
@@ -18,17 +21,38 @@ class Sketch : NSObject {
         // Set starting position
         x = 250
         
+        // Set the difference for x
+        offset = 1
+        
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        // Increasing the offset
+        offset += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        // Draw an ellipse 1
+        canvas.fillColor = Color.yellow
+        canvas.borderColor = Color.yellow
+        canvas.drawEllipse(centreX: x - offset, centreY: x - offset, width: 50, height: 50)
+        
+        // Draw an ellipse 2
+        canvas.fillColor = Color.blue
+        canvas.borderColor = Color.blue
+        canvas.drawEllipse(centreX: x + offset, centreY: x - offset, width: 50, height: 50)
+        
+        // Draw an ellipse 3
+        canvas.fillColor = Color.green
+        canvas.borderColor = Color.green
+        canvas.drawEllipse(centreX: x - offset, centreY: x + offset, width: 50, height: 50)
+        
+        // Draw an ellipse 4
+        canvas.fillColor = Color.red
+        canvas.borderColor = Color.red
+        canvas.drawEllipse(centreX: x + offset, centreY: x + offset, width: 50, height: 50)
         
     }
     
 }
+
