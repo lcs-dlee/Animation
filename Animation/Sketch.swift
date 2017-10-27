b4b7826f4973423d8b2ca745fd6de2467f7be7c0
Lee, Dakyum <dlee@lcs-mba3088.lcs.on.ca>
2017-10-27 14:11:10 -0400

    Draw two circles

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,11 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
+    
+    // Change of position
+    var dx : Int
+    var dy : Int
     
     // This function runs once
     override init() {
@@ -16,19 +21,57 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
+        y = 0
         
+        // Set change in position
+        dx = 2
+        dy = 1
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
         // Change position
-        x += 1
+        x += dx
+        y += dy
+    
+        // Clear the background
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        // Draw a bigger ellipse in the middle of the canvas
+        canvas.fillColor = Color.white
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 250, height: 250, borderWidth: 3)
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 3, height: 3)
+        
+        // Draw a smaller ellipse in the middle of the canvas
+        canvas.fillColor = Color.white
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50, borderWidth: 3)
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: y, width: 3, height: 3)
+        
+        // Check the position_Bounce at the right edge
+        if x > 500{
+            dx = -2
+        }
+        
+        // Check the position_Bounce at the left edge
+        if x < 0{
+            dx = 2
+            
+        }
+        
+        // Check the position_Bounce at the top edge
+        if y > 500{
+            dy = -1
+        }
+    
+        // Check the position_Bounce at the bottom edge
+        if y < 0{
+            dy = 1
+        }
         
     }
-    
 }
